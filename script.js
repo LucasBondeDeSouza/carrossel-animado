154d43a8c55d8177d1d9d04d086b37a9ee4afad6
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-06-01 14:15:12 -0300

    atualização

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-let btnNext = document.querySelector('.next')
-let btnBack = document.querySelector('.back')
+let btnNext = document.querySelector('.next');
+let btnBack = document.querySelector('.back');
 
-let container = document.querySelector('.container')
-let list = document.querySelector('.container .list')
-let thumb = document.querySelector('.container .thumb')
+let container = document.querySelector('.container');
+let list = document.querySelector('.container .list');
+let thumb = document.querySelector('.container .thumb');
 
-btnNext.onclick = () => moveItensOnClick('next')
-btnBack.onclick = () => moveItensOnClick('back')
+let intervalId;
+
+btnNext.onclick = () => {
+    moveItensOnClick('next');
+    resetCarousel();
+};
+btnBack.onclick = () => {
+    moveItensOnClick('back');
+    resetCarousel();
+};
 
 function moveItensOnClick(type) {
-    let listItems = document.querySelectorAll('.list .list-item')
-    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
+    let listItems = document.querySelectorAll('.list .list-item');
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item');
 
     if (type === 'next') {
-        list.appendChild(listItems[0])
-        thumb.appendChild(thumbItems[0])
-        container.classList.add('next')
+        list.appendChild(listItems[0]);
+        thumb.appendChild(thumbItems[0]);
+        container.classList.add('next');
     } else {
-        list.prepend(listItems[listItems.length - 1])
-        thumb.prepend(thumbItems[thumbItems.length - 1])
-        container.classList.add('back')
+        list.prepend(listItems[listItems.length - 1]);
+        thumb.prepend(thumbItems[thumbItems.length - 1]);
+        container.classList.add('back');
     }
 
     setTimeout(() => {
-        container.classList.remove('next')
-        container.classList.remove('back')
-    }, 3000)
+        container.classList.remove('next');
+        container.classList.remove('back');
+    }, 5000);
 }
+
+// Função para iniciar o carrossel automático
+function startCarousel() {
+    intervalId = setInterval(() => {
+        moveItensOnClick('next');
+    }, 5000);
+}
+
+// Função para resetar o carrossel
+function resetCarousel() {
+    clearInterval(intervalId);
+    startCarousel();
+}
+
+// Iniciar o carrossel automático
+startCarousel();
